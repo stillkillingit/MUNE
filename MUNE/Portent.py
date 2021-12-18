5fb7993cbac1e9103301798cf9557f1a9876af78
stillkillingit <ross.a.cook@gmail.com>
2021-12-18 15:08:29 -0600

    update

--- MUNE/Portent.py
+++ MUNE/Portent.py
@@ -1,20 +1,16 @@
 # Create functions for Portent, Interventions, Portents, NPC Attitudes,
 #    and TWENE.  Ideally, integrate these with a basic NPC generation 
-from selenium import webdriver
-import math
-import random # a = random.randint(start,end)
+from requests_html import HTMLSession
 
 class Portent():
     def portent_1(self):
-       driver = webdriver.Chrome()
-       driver.get('http://watchout4snakes.com/Random/RandomSentence')
-       omen = driver.find_element_by_id('result').text
-       driver.close()
+       session = HTMLSession()
+       r = session.get('http://watchout4snakes.com/Random/RandomSentence')
+       
+       # Add sleep time; default is not long enough
+       r.html.render(sleep=0.5)
+       omen = r.html.fine('#result', first=TRUE).text
        return omen
-
-    # Function for importing number
-    #def foreshadow(self, omen):
-    #    return getattr(self, omen)()
 
 # Creating the Portent object
 trelawney = Portent()
