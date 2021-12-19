0c8625114c1ff5626dd709799b65f89e16e724e4
stillkillingit <ross.a.cook@gmail.com>
2021-12-18 19:38:49 -0600

    Working

--- MUNE/Portent.py
+++ MUNE/Portent.py
@@ -1,16 +1,30 @@
 # Create functions for Portent, Interventions, Portents, NPC Attitudes,
 #    and TWENE.  Ideally, integrate these with a basic NPC generation 
-from requests_html import HTMLSession
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 class Portent():
-    def portent_1(self):
-       session = HTMLSession()
-       r = session.get('http://watchout4snakes.com/Random/RandomSentence')
-       
-       # Add sleep time; default is not long enough
-       r.html.render(sleep=0.5)
-       omen = r.html.fine('#result', first=TRUE).text
-       return omen
+
+    def Omen(self):
+        
+        option = webdriver.ChromeOptions()
+        option.add_argument('--headless')
+        option.add_argument('--no-sandbox')
+        option.add_argument('--disable-dev-sh-usage')
+        path = Service('C:\\Users\\Ross\\AppData\\Local\\Programs\\Python\\Python310\\chromedriver.exe')
+        driver = webdriver.Chrome(service=path, options=option)
+        #driver = webdriver.Firefox()
+        url = 'http://watchout4snakes.com/Random/RandomSentence'
+        driver.get(url)
+        omen = driver.find_element(By.ID,"result").text
+
+        return omen
+
+    def __repr__(self) -> str:
+        return self.Omen()
 
 # Creating the Portent object
 trelawney = Portent()
